fee4f94a996f92fd644801f85839acda2931a171
Julia Guenther <juliag1@alumni.stanford.edu>
2016-03-06 20:24:11 -0800

    Successfully creating DataFrames using spark-csv

--- ratings.py
+++ ratings.py
@@ -8,32 +8,27 @@ sc = SparkContext(conf = conf)
 #Creates a new Spark SQL 
 sqlContext = SQLContext(sc)
 
-# Load a text file and convert each line to a tuple.
-ratingsLines = sc.textFile("./ml-20m/ratings.csv")
-parts = ratingsLines.map(lambda l: l.split(","))
-ratings = parts.map(lambda p: (p[0], p[1], p[2], p[3].strip()))
+ratingsSchema = StructType([ \
+    StructField("userId", StringType(), True), \
+    StructField("movieId", StringType(), True), \
+    StructField("rating", StringType(), True), \
+    StructField("timestamp", StringType(), True)])
 
-# The schema is encoded in a string.
-ratingsSchemaString = "userId movieId rating timestamp"
+# Load a text file and create a new DataFrame from the text file 
+ratingsDF = sqlContext.read.format('com.databricks.spark.csv').options(quote="\"").load('./ml-20m/ratings.csv', schema=ratingsSchema)
+ratingsDF.registerTempTable("ratings")
 
-ratingsFields = [StructField(fieldName, StringType(), True) for fieldName in ratingsSchemaString.split()]
-ratingsSchema = StructType(ratingsFields)
+moviesSchema = StructType([ \
+    StructField("movieId", StringType(), True), \
+    StructField("title", StringType(), True), \
+    StructField("genres", StringType(), True)])
 
-# Apply the schema to the RDD.
-schemaRatings = sqlContext.createDataFrame(ratings, ratingsSchema)
-
-# Register the DataFrame as a table.
-schemaRatings.registerTempTable("ratings")
-
-# movieLines = sc.textFile("./ml-20m/movies.csv")
-# parts = ratingsLines.map(lambda l: l.split(","))
-# ratings = parts.map(lambda p: (p[0], p[1], p[2].strip()))
+moviesDF = sqlContext.read.format('com.databricks.spark.csv').options(quote="\"").load('./ml-20m/movies.csv', schema=moviesSchema)
+moviesDF.registerTempTable("movies")
 
 
-# SQL can be run over DataFrames that have been registered as a table.
-results = sqlContext.sql("SELECT userId FROM ratings")
+results = sqlContext.sql("SELECT * FROM movies")
 
-# The results of SQL queries are RDDs and support all the normal RDD operations.
-ids = results.map(lambda p: "UserID: " + p.userId)
-for userId in ids.collect():
-  print(userId)
+types = results.map(lambda p:"Movie ID "+p.movieId+ " Genres: " + p.genres)
+for genre in types.collect():
+  print(genre)
