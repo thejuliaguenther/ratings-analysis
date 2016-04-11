a55b1fd96f5c7bce02f82463315d1b12d6ceef44
Julia Guenther <juliag1@alumni.stanford.edu>
2016-04-10 22:25:40 -0700

    Created route for movie page

--- server.py
+++ server.py
@@ -13,6 +13,10 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     return render_template("index.html")
 
+@app.route('/movies', methods=["GET"])
+def display_movies():
+    return render_template("movie.html")
+
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 5000))
     DEBUG = "NO_DEBUG" not in os.environ
