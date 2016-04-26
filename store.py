be0848e5d891f9f742753ef8bd22d12d53c193ef
Julia Guenther <juliag1@alumni.stanford.edu>
2016-04-25 23:25:40 -0700

    Successfully added all movie ids and titles to redis

--- store.py
+++ store.py
@@ -7,5 +7,5 @@ movie_data = open("movies.json").read()
 movies_to_load = movie_data.encode('ascii', 'ignore')
 movie_json = json.loads(movies_to_load)
 
-print movie_json
-# r.set('foo', 'bar')
+for line in movie_json:
+    r.set(str(line), str(movie_json[line]))
