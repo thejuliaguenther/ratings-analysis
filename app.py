863a67b188832a3bfeb3c0251d5c726a96313677
Julia Guenther <juliag1@alumni.stanford.edu>
2016-04-26 19:31:49 -0700

    Cleaned tags

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def remove_duplicate_tags(lst):
 
     for i in xrange(len(split_lst)):
         tag = split_lst[i].strip()
+        if i == 0:
+            tag = tag[1:]
+        if i == (len(split_lst)-1):
+            tag = tag[:-1]
+        tag = tag[1:-1]
         if tag not in seen:
             result.append(tag)
             seen.add(tag)
