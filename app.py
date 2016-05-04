06aa4c4ec0685308b6c4caca5747d6a641120677
Julia Guenther <juliag1@alumni.stanford.edu>
2016-05-04 12:59:35 -0700

    Now formatting date JSON for graph

--- app.py
+++ app.py
@@ -39,8 +39,19 @@ def process_timestamps(rating_dates):
         date_year = new_date.year
         date_month = new_date.month
 
-        if (date_month,date_year) in month_year_count:
-            month_year_count[(date_month,date_year)] += 1
+        date_string = str(date_month) + "-"+ str(date_year)
+
+        if date_string in month_year_count:
+            month_year_count[date_string] += 1
         else:
-            month_year_count[(date_month,date_year)] = 1
-    return month_year_count
+            month_year_count[date_string] = 1
+    key_value_list = get_keys_and_values(month_year_count)
+    return key_value_list
+
+def get_keys_and_values(count_dict):
+    result = []
+    for key in count_dict:
+        pair = {'month':key, 'count':count_dict[key]}
+        result.append(pair)
+    return result 
+
