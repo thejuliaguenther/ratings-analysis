26b6e7003701b96f190bffdea982d9982a756116
Julia Guenther <juliag1@alumni.stanford.edu>
2016-05-16 16:26:02 -0700

    Eliminated empty string from prefixes

--- store.py
+++ store.py
@@ -91,9 +91,15 @@ r8 = redis.StrictRedis(host='localhost', port=6379, db=7)
 
 for x in movie_json:
     movie_name = x[1]
+    movie_id = x[0]
+    prefixes = []
     for letter in xrange(len(movie_name[0:-6])):
         prefix = movie_name[0:letter]
-        print prefix
+        if prefix == "":
+            continue
+        prefixes.append(prefix.encode('ascii', 'ignore'))
+    r8.set(movie_id, prefixes)
+    print r8.get(movie_id)
     #     r8.zadd('substrings',0,prefix)
     # r8.zadd('substrings',0,movie_name+'*')
     # print r8.smembers('substrings')
